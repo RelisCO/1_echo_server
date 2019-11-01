7cc20a993f88dfe05076e7f0af8b035c0eefffe5
Relisiler <demidovnik01@gmail.com>
2019-11-01 09:59:24 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,16 +1,46 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+# Функции получения и отправки сообщения
+def sending(sock, msg):
+    sock.send(("{0:0>10}".format(len(msg)) + msg).encode())    
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def receiving(sock):
+    try: # Если клиент отключился получим пустое сообщение
+        length = int(sock.recv(10).decode()) # Поэтому ловим ошибку перевода пустого сообщения в int
+    except: # Для клиента это бесполезно, но функция скопированна из сервера и зачем её менять
+        return None # И так работает
+    msg = sock.recv(length).decode()
+    return msg
 
-data = sock.recv(1024)
 
-sock.close()
+sock = socket.socket() # Создаём соке
 
-print(data.decode())
+host = input("Введите имя хоста: ")
+if host == "": host = "localhost"
+while True:
+    port = input("Введите номер порта: ")
+    if port == "": 
+        port = 9090
+        break
+    else:
+        try:
+            port = int(port)
+        except:
+            print("Номер порта - целое число")
+
+sock.connect((host, port)) # Подключаемся к серверу
+print("Соединение с сервером")
+
+
+print("Для выхода введите exit")
+while True:
+    msg = input() # Считываем сообщение
+    if msg == 'exit': # Если сообщение exit - выходим из цикла
+        break
+    sending(sock, msg) # Отправляем сообщение на сервер
+    print("Отправка данных серверу")
+    data = receiving(sock) # Получаем данные с сервера
+    print("Получение данных от сервера")
+    print(data) # Выводим полученные данные
+
+sock.close() # Закрываем сокет
--- server.py
+++ server.py
@@ -1,20 +1,59 @@
 import socket
+import errno
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+# Функции получения и отправки сообщения
+def sending(sock, msg):
+    sock.send(("{0:0>10}".format(len(msg)) + msg).encode())
+    
+def receiving(sock):
+    try: # Если клиент отключился получим пустое сообщение
+        length = int(sock.recv(10).decode()) # Поэтому ловим ошибку перевода пустого сообщения в int
+    except:
+        return None
+    msg = sock.recv(length).decode()
+    return msg
 
-msg = ''
+
+sock = socket.socket() # Создаём сокет
+port = 9090 # Стартовое значение порта
+while True:
+    try:
+        sock.bind(('', port)) # Задаём хост и порт
+    except socket.error as e:
+        if e.errno == errno.EADDRINUSE:
+            port += 1 # Если порт занят увеличиваем его на единицу
+    else:
+        break
+print(f"Слушаю порт {port}")    
+
+    
+with open('log.txt', 'a') as file:
+    file.write("Запуск сервера\n")
+sock.listen(1) # Начинаем прослушивание
+with open('log.txt', 'a') as file:
+    file.write("Начало прослушивания\n")
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    try:
+        conn, addr = sock.accept() # Принимаем подключение
+        with open('log.txt', 'a') as file:
+            file.write("Подключение клиента\n")
+        while True:
+            data = receiving(conn) # Получаем сообщение от клиента
+            with open('log.txt', 'a') as file:
+                file.write("Получение данных от клинета\n")
+            if not data:
+                break # Если сообщения нет - клинет отключился, значит выходим из цикла
+            sending(conn, data) # Отправляем клиенту его сообщение обратно
+            with open('log.txt', 'a') as file:
+                file.write("Отправка данных клиенту\n")
+            
+        conn.close() # Закрываем соединение
+        with open('log.txt', 'a') as file:
+            file.write("Отключение клиента\n")
+    except KeyboardInterrupt:
+        break
+sock.close() # Закрываем сокет
+with open('log.txt', 'a') as file:
+    file.write("Закрытие сервера\n")
+    
